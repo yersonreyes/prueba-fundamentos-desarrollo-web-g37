f0337152b1e62516ef86b8d38d63602e3c608d19
Yerson Reyes <yerson.programador@gmail.com>
2022-09-12 10:05:34 -0400

    First Commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$("#contact-form").submit(function (event) {
+  event.preventDefault();
+  console.log("hola mundo");
+
+  var nombre = $("#inputNombre").val();
+  var asunto = $("#inputAsunto").val();
+  var mensaje = $("#inputMensaje").val();
+
+  console.log("estos son los datos capturados del formulario");
+  console.log({ nombre, asunto, mensaje });
+
+  setTimeout(function () {
+    $("#inputNombre").val("");
+    $("#inputAsunto").val("");
+    $("#inputMensaje").val("");
+    alert("mensaje enviado con exito, pronto alguien te contactara");
+  }, 2000);
+});
+
+$(".title").click(function (event) {
+  var titleElement = event.target;
+  $(titleElement).toggleClass("text-warning");
+});
